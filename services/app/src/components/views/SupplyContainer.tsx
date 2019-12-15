d9038cc11f5e6052b0169a553daa5923d9ee0c1e
Todd Medema <toddmedema@gmail.com>
2019-12-14 20:40:18 -0500

    can sell generators
    Former-commit-id: 653bf0922053886d0286531e3fc1c90d9d2d4f44 [formerly 31e3fb241f342bc667e945a804a7400dabd03a40]
    Former-commit-id: 3d3ca03a1c631093ca16d0a38e0b8e75c64f415f

--- services/app/src/components/views/SupplyContainer.tsx
+++ services/app/src/components/views/SupplyContainer.tsx
@@ -1,18 +1,22 @@
 import {connect} from 'react-redux';
 import Redux from 'redux';
-import {AppStateType, GeneratorType} from '../../Types';
+import {AppStateType, BuildGeneratorAction, GeneratorShoppingType, SellGeneratorAction} from '../../Types';
 import Supply, {DispatchProps, StateProps} from './Supply';
 
 const mapStateToProps = (state: AppStateType): StateProps => {
   return {
+    cash: state.gameState.cash,
     generators: state.gameState.generators,
   };
 };
 
 const mapDispatchToProps = (dispatch: Redux.Dispatch<any>): DispatchProps => {
   return {
-    onBuildGenerator: (generator: GeneratorType) => {
-      dispatch({type: 'BUILD_GENERATOR', generator});
+    onBuildGenerator: (generator: GeneratorShoppingType) => {
+      dispatch({type: 'BUILD_GENERATOR', generator} as BuildGeneratorAction);
+    },
+    onSellGenerator: (id: number) => {
+      dispatch({type: 'SELL_GENERATOR', id} as SellGeneratorAction);
     },
   };
 };
