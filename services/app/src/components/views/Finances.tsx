6bf213b33ef99af47efd3a187edc06d4f85ac440
Todd Medema <toddmedema@gmail.com>
2019-12-17 22:24:23 -0500

    initial fuel setup
    Former-commit-id: ba6c1a36d0654d996f4ba9b16f4a2d80b709d8d3 [formerly cfaf95c31b72f8a3c007249501527b58f21f68fa]
    Former-commit-id: ba9c34ab62f0470e1b51157499c1d46847bb28a0

--- services/app/src/components/views/Finances.tsx
+++ services/app/src/components/views/Finances.tsx
@@ -1,9 +1,12 @@
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
+import {getDateFromMinute} from 'shared/helpers/DateTime';
+import {GameStateType} from '../../Types';
 import BuildCard from '../base/BuildCard';
 
 export interface StateProps {
+  gameState: GameStateType;
 }
 
 export interface DispatchProps {
@@ -12,10 +15,11 @@ export interface DispatchProps {
 export interface Props extends StateProps, DispatchProps {}
 
 const FinancesBuild = (props: Props): JSX.Element => {
+  const date = getDateFromMinute(props.gameState.currentMinute);
   return (
     <BuildCard className="Finances">
       <Toolbar>
-        <Typography variant="h6">Finances</Typography>
+        <Typography variant="h6">{date.year} Finances</Typography>
       </Toolbar>
       <div className="scrollable">
         Numbers...
