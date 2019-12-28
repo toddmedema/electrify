d230ac8ae19a079b14e0995c1961b0efd20ef96d
Todd Medema <toddmedema@gmail.com>
2019-12-27 22:16:20 -0500

    MVP battery storage
    Former-commit-id: e0d76dede1583d0e0872fcf348a6d45ad205956a [formerly 94a9b23256165ebf9bbc7280446e549f7e160bab]
    Former-commit-id: 5ece6f168a1846bd3ecaa110bf15596ec4a463d0

--- services/app/src/components/views/StorageContainer.tsx
+++ services/app/src/components/views/StorageContainer.tsx
@@ -1,15 +1,26 @@
 import {connect} from 'react-redux';
 import Redux from 'redux';
-import {AppStateType} from '../../Types';
+import {AppStateType, BuildStorageAction, ReprioritizeStorageAction, SellStorageAction, StorageShoppingType} from '../../Types';
 import Storage, {DispatchProps, StateProps} from './Storage';
 
 const mapStateToProps = (state: AppStateType): StateProps => {
   return {
+    gameState: state.gameState,
+    cash: (state.gameState.monthlyHistory[0] || {}).cash,
   };
 };
 
 const mapDispatchToProps = (dispatch: Redux.Dispatch<any>): DispatchProps => {
   return {
+    onBuildStorage: (storage: StorageShoppingType, financed: boolean) => {
+      dispatch({type: 'BUILD_STORAGE', storage, financed} as BuildStorageAction);
+    },
+    onSellStorage: (id: number) => {
+      dispatch({type: 'SELL_STORAGE', id} as SellStorageAction);
+    },
+    onReprioritizeStorage: (spotInList: number, delta: number) => {
+      dispatch({type: 'REPRIORITIZE_STORAGE', spotInList, delta} as ReprioritizeStorageAction);
+    },
   };
 };
 
