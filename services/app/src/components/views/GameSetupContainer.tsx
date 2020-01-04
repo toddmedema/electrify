c9d122f83a8d2d57cc38f784054d733ab88247f4
Todd Medema <toddmedema@gmail.com>
2020-01-03 22:35:05 -0500

    add game difficulty
    Former-commit-id: 9b89a0b0f4a64f909699dbadb207d35f73aefa71 [formerly 7042c6c9f7e362331dc739ce55ec52bd3a5093cb]
    Former-commit-id: a680091545904e1a8188286187a4f46d6461cb3c

--- services/app/src/components/views/GameSetupContainer.tsx
+++ services/app/src/components/views/GameSetupContainer.tsx
@@ -1,15 +1,23 @@
+import {quitGame} from 'app/reducers/GameState';
 import {connect} from 'react-redux';
 import Redux from 'redux';
-import {AppStateType} from '../../Types';
+import {AppStateType, GameStateType} from '../../Types';
 import GameSetup, {DispatchProps, StateProps} from './GameSetup';
 
 const mapStateToProps = (state: AppStateType): StateProps => {
   return {
+    gameState: state.gameState,
   };
 };
 
 const mapDispatchToProps = (dispatch: Redux.Dispatch<any>): DispatchProps => {
   return {
+    onBack: () => {
+      dispatch(quitGame());
+    },
+    onDelta: (delta: Partial<GameStateType>) => {
+      dispatch({type: 'GAMESTATE_DELTA', delta});
+    },
     onStart: () => {
       dispatch({type: 'GAME_START'});
     },
