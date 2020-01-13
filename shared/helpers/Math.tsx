d1cb8a494129d28d2269ccd4e89ae74bcc503f60
Todd Medema <toddmedema@gmail.com>
2020-01-12 22:41:02 -0500

    tabular data for forecast blackouts
    Former-commit-id: 2587f3ee964ea7d2322356946ee7602c601db6b5 [formerly bbbfd083f949588af56d1ac6a05533532937eb6e]
    Former-commit-id: 50ec33c00c3bc6af62d579d90887626996633b44

---
+++ shared/helpers/Math.tsx
@@ -0,0 +1,11 @@
+// http://jsfiddle.net/justin_c_rounds/Gd2S2/light/
+export function getIntersectionX(line1StartX: number, line1StartY: number, line1EndX: number, line1EndY: number, line2StartX: number, line2StartY: number, line2EndX: number, line2EndY: number) {
+  const denominator = ((line2EndY - line2StartY) * (line1EndX - line1StartX)) - ((line2EndX - line2StartX) * (line1EndY - line1StartY));
+  if (denominator === 0) {
+    return 0;
+  }
+  const a = line1StartY - line2StartY;
+  const b = line1StartX - line2StartX;
+  const numerator1 = ((line2EndX - line2StartX) * a) - ((line2EndY - line2StartY) * b);
+  return line1StartX + (numerator1 / denominator * (line1EndX - line1StartX));
+}
