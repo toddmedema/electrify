7a94d629b6b411dfb97cefa342893377edecec0b
Todd Medema <toddmedema@gmail.com>
2020-01-15 09:22:54 -0500

    rename and expand difficulty levels
    Former-commit-id: 9854629850baaa7874ea83620b83fd0c1d30bde5 [formerly 906915c46c89de96a4702f1120d7f66207e5ee6b]
    Former-commit-id: b7b4c56f64756b5ba961c17888784d17d6ab936b

--- services/app/src/components/views/SettingsContainer.tsx
+++ services/app/src/components/views/SettingsContainer.tsx
@@ -2,7 +2,7 @@ import {connect} from 'react-redux';
 import Redux from 'redux';
 import {toCard} from '../../actions/Card';
 import {changeSettings} from '../../actions/Settings';
-import {AppStateType, DifficultyType} from '../../Types';
+import {AppStateType} from '../../Types';
 import Settings, {DispatchProps, StateProps} from './Settings';
 
 const mapStateToProps = (state: AppStateType): StateProps => {
@@ -11,34 +11,10 @@ const mapStateToProps = (state: AppStateType): StateProps => {
   };
 };
 
-/* tslint:disable */
-const difficultyAdd: any = {
-  EASY: 'NORMAL',
-  NORMAL: 'HARD',
-  HARD: 'IMPOSSIBLE',
-  IMPOSSIBLE: 'EASY',
-};
-
-const difficultySub: any = {
-  EASY: 'IMPOSSIBLE',
-  NORMAL: 'EASY',
-  HARD: 'NORMAL',
-  IMPOSSIBLE: 'HARD',
-};
-/* tslint:enable */
-
 const mapDispatchToProps = (dispatch: Redux.Dispatch<any>): DispatchProps => {
   return {
     onAudioChange: (v: boolean) => {
       dispatch(changeSettings({audioEnabled: v}));
-    },
-    onDifficultyDelta: (difficulty: DifficultyType, i: number) => {
-      if (i > 0) {
-        difficulty = difficultyAdd[difficulty];
-      } else {
-        difficulty = difficultySub[difficulty];
-      }
-      dispatch(changeSettings({difficulty}));
     },
     onExperimentalChange: (v: boolean) => {
       dispatch(changeSettings({experimental: v}));
