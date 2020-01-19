18fd3003db3f365ae5fb75e37a4063d93dc593e8
Todd Medema <toddmedema@gmail.com>
2020-01-18 21:20:28 -0500

    LCOE correctly factors in carbon fee
    Former-commit-id: 72637abfc53e9f49e165951ae900feb23feaadd3 [formerly 6357daf91ffc8684b5515d9f5bd544b23b90a2da]
    Former-commit-id: 5427a9fae26b8683ba4e2cc91ebbf28cdd17d19d

--- shared/helpers/Financials.tsx
+++ shared/helpers/Financials.tsx
@@ -16,7 +16,7 @@ export function getPaymentInterest(balance: number, interestRate: number, monthl
 
 export function LCWH(g: GeneratorShoppingType, feePerKgCO2e: number) {
   const fuel = FUELS[g.fuel] || {};
-  const fuelCostPerWh = (fuel.costPerBtu || 0 + feePerKgCO2e * fuel.kgCO2ePerBtu || 0) * g.btuPerWh;
+  const fuelCostPerWh = ((fuel.costPerBtu || 0) + (feePerKgCO2e * fuel.kgCO2ePerBtu || 0)) * g.btuPerWh;
   const totalWh = g.peakW * g.lifespanYears * HOURS_PER_YEAR_REAL * g.capacityFactor;
   const costPerWh = (g.buildCost + g.annualOperatingCost * g.lifespanYears + fuelCostPerWh * totalWh) / totalWh;
   return costPerWh;
