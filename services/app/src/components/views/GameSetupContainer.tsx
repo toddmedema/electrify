a4657e54aeea8f520c7ed5e497214dbc97795809
Todd Medema <toddmedema@gmail.com>
2020-01-25 13:56:06 -0500

    learning to play scenarios MVP
    Former-commit-id: 5f40a1af341d27e10765ec51799920843afc411b [formerly dad33a665390b305a9c4f51ae012166cc51a02e1]
    Former-commit-id: 65f94c13a1e4f10b92b593ec7faa4e0d9b0c28c9

--- services/app/src/components/views/GameSetupContainer.tsx
+++ services/app/src/components/views/GameSetupContainer.tsx
@@ -2,7 +2,7 @@ import {quitGame} from 'app/reducers/GameState';
 import {connect} from 'react-redux';
 import Redux from 'redux';
 import {openDialog} from '../../actions/UI';
-import {AppStateType, DialogType, GameStateType} from '../../Types';
+import {AppStateType, DialogType, GameStateType, StartGameAction} from '../../Types';
 import GameSetup, {DispatchProps, StateProps} from './GameSetup';
 
 const mapStateToProps = (state: AppStateType): StateProps => {
@@ -20,7 +20,7 @@ const mapDispatchToProps = (dispatch: Redux.Dispatch<any>): DispatchProps => {
       dispatch({type: 'GAMESTATE_DELTA', delta});
     },
     onStart: () => {
-      dispatch({type: 'GAME_START'});
+      dispatch({type: 'GAME_START'} as StartGameAction);
     },
     openDialog: (dialog: DialogType) => {
       dispatch(openDialog(dialog));
