b71506b05f6a3db74c9185616e7913cfe078bb49
Todd Medema <toddmedema@gmail.com>
2020-01-26 23:38:16 -0500

    different starting facilities for scenarios, improved styling of scenario list
    Former-commit-id: fef0b61dd6ee31fd081aeb7dcff3a8329ef36b20 [formerly 099e14a18c86ca2dc7c049b6a827d51e702848ab]
    Former-commit-id: de7684aa029502f0f2ee760bddfae877eb6906d8

--- services/app/src/components/views/LoadingContainer.tsx
+++ services/app/src/components/views/LoadingContainer.tsx
@@ -16,23 +16,26 @@ const mapStateToProps = (state: AppStateType): StateProps => {
 const mapDispatchToProps = (dispatch: Redux.Dispatch<any>): DispatchProps => {
   return {
     load: (gameState: GameStateType) => {
+      const scenario = SCENARIOS.find((s) => s.id === gameState.scenarioId);
+      if (!scenario) {
+        return alert('Unknown scenario ID');
+      }
       initWeather('SF', () => {
         // TODO load game state from localstorage if loading
-        // TODO cash based on starting year (gameState.date.year)
 
         initFuelPrices(() => {
           // Otherwise, generate from scratch
           // TODO different scenarios - for example, start with Natural Gas if year is 2000+, otherwise coal
           dispatch({
             type: 'NEW_GAME',
-            facilities: [{fuel: 'Natural Gas', peakW: 500000000}],
+            facilities: scenario.facilities,
             cash: 200000000,
             population: 1080000,
           } as NewGameAction);
 
           dispatch({type: 'GAME_LOADED'});
 
-          if ((SCENARIOS.find((s) => s.id === gameState.scenarioId) || {}).tutorialSteps) {
+          if (scenario.tutorialSteps) {
             setTimeout(() => dispatch({type: 'GAMESTATE_DELTA', delta: {tutorialStep: 0}}), 300);
           }
         });
