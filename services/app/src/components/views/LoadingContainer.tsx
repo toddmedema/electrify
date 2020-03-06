dd820fbd27c41b963630a6fe34ebb07f18a1d742
Todd Medema <toddmedema@gmail.com>
2020-03-05 23:41:00 -0500

    add pit weather and scenarios - save point (SF broken)
    Former-commit-id: e1a4a8e66f2f17a59581e9c692682a2f027ca023 [formerly a20639117671f3f732da694da1da370560f4e7dd]
    Former-commit-id: d289ece8b3df432df103883afccc9ec19053ce2f

--- services/app/src/components/views/LoadingContainer.tsx
+++ services/app/src/components/views/LoadingContainer.tsx
@@ -4,6 +4,7 @@ import Redux from 'redux';
 import {logEvent} from 'app/Globals';
 import {initFuelPrices} from 'shared/schema/FuelPrices';
 import {initWeather} from 'shared/schema/Weather';
+import {LOCATIONS} from '../../Constants';
 import {SCENARIOS} from '../../Scenarios';
 import {AppStateType, GameStateType, NewGameAction} from '../../Types';
 import Loading, {DispatchProps, StateProps} from './Loading';
@@ -17,12 +18,17 @@ const mapStateToProps = (state: AppStateType): StateProps => {
 const mapDispatchToProps = (dispatch: Redux.Dispatch<any>): DispatchProps => {
   return {
     load: (gameState: GameStateType) => {
-      logEvent('scenario_start', {id: gameState.scenarioId});
+      logEvent('scenario_start', {id: gameState.scenarioId, difficulty: gameState.difficulty});
       const scenario = SCENARIOS.find((s) => s.id === gameState.scenarioId);
       if (!scenario) {
-        return alert('Unknown scenario ID');
+        return alert('Unknown scenario ID ' + gameState.scenarioId);
       }
-      initWeather('SF', () => {
+      const location = LOCATIONS.find((s) => s.id === scenario.locationId);
+      if (!location) {
+        return alert('Unknown location ID ' + scenario.locationId);
+      }
+
+      initWeather(location.id, () => {
         // TODO load game state from localstorage if loading
 
         initFuelPrices(() => {
@@ -33,6 +39,7 @@ const mapDispatchToProps = (dispatch: Redux.Dispatch<any>): DispatchProps => {
             facilities: scenario.facilities,
             cash: 200000000,
             customers: 1030000,
+            location,
           } as NewGameAction);
 
           dispatch({type: 'GAME_LOADED'});
