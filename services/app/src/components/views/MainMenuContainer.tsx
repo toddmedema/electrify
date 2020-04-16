cda3146d734c10ac6c18181f9506455b9ab0e645
Todd Medema <toddmedema@gmail.com>
2020-04-16 09:29:11 -0400

    opt in to music
    Former-commit-id: f499706dd2d3b6b19d2bb231d37d721759c5d45c [formerly 1b5099572fa0b6185bcd31e1ab0fc0a47e28034f]
    Former-commit-id: e0299775b424c5506411b0fda4d510a86f01cacf

--- services/app/src/components/views/MainMenuContainer.tsx
+++ services/app/src/components/views/MainMenuContainer.tsx
@@ -3,15 +3,20 @@ import {AppStateType} from 'app/Types';
 import {connect} from 'react-redux';
 import Redux from 'redux';
 import {toCard} from '../../actions/Card';
+import {changeSettings} from '../../actions/Settings';
 import MainMenu, {DispatchProps, StateProps} from './MainMenu';
 
 const mapStateToProps = (state: AppStateType): StateProps => {
   return {
+    audioEnabled: state.settings.audioEnabled,
   };
 };
 
 const mapDispatchToProps = (dispatch: Redux.Dispatch<any>): DispatchProps => {
   return {
+    onAudioChange: (v: boolean) => {
+      dispatch(changeSettings({audioEnabled: v}));
+    },
     onManual: () => {
       dispatch(toCard({name: 'MANUAL'}));
     },
