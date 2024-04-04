cd5cda6a8b9322b237310b98069eca6429287c69
Todd Medema <toddmedema@gmail.com>
2024-04-03 19:12:25 -0700

    add tests for formatWatts

--- src/helpers/Format.tsx
+++ src/helpers/Format.tsx
@@ -1,12 +1,22 @@
 const numbro = require('numbro');
 
+/**
+ * This function formats a number representing watts into a string with appropriate units.
+ * It uses the numbro library to format the number with a specified mantissa and a maximum length of max(2, mantissa).
+ * The function also replaces certain characters to use the correct abbreviations for MegaWatts, GigaWatts, and TeraWatts.
+ * The result is appended with 'W' to indicate watts.
+ * 
+ * @param {number} i - The number to be formatted.
+ * @param {number} mantissa - The number of significant digits to display after the decimal point. Default is 1.
+ * @returns {string} - The formatted string representing the number in watts with appropriate units.
+ */
 export function formatWatts(i: number, mantissa = 1): string {
   return numbro(i)
     .format({
       spaceSeparated: false,
       average: true,
       trimMantissa: true,
-      totalLength: 2,
+      totalLength: Math.max(2, mantissa),
       mantissa,
     })
     // lowercase k for thousands in both cases
